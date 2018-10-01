05564c30b9473dc5628d958af5a444f8e5ce4aa0
Sebastian Krawczyk <s.krawczyk84@gmx.de>
2018-10-01 11:22:05 +0200

    Delete publishVersion.sh

--- publishVersion.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# slightly different on mac:
-# sed -i "" "1s/.*/\[\!\[Version\](https:\/\/img\.shields\.io\/badge\/style-$TRAVIS_BUILD_NUMBER-green\.svg\?style=flat\&label=version)\]()/" README.md
-sed -i "1s/.*/\[\!\[Version\](https:\/\/img\.shields\.io\/badge\/style-$TRAVIS_BUILD_NUMBER-green\.svg\?style=flat\&label=version)\]()/" README.md
-git checkout -b __temp
-git add README.md
-git commit -m "[ci skip] update version badge in README"
-git push origin __temp:master
